6e18984e0601977221641d76c097096ad625b741
Viacheslav <slavikap19@gmail.com>
2024-02-20 13:14:35 +0100

    update js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,31 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+
 export default function renderCard(cards) {
     const gallery = document.querySelector('.cards');
-          cards.forEach(card => {
-            const cardHTML =
-                `<li class="card"> 
-                    <a class="card-link" href="${card.largeImageURL}">
-                        <img class="card-img" src="${card.webformatURL}" alt="${card.tags}" data-source="${card.largeImageURL}" />
-                    </a>
-                    <div class="card-data">
-                        <div class="data-wrap">
-                            <p>Likes: </p>
-                            <span>${card.likes}</span>
-                        </div>
-                        <div class="data-wrap">
-                            <p>Views: </p>
-                            <span>${card.views}</span>
-                        </div>
-                        <div class="data-wrap">
-                            <p>Comments: </p>
-                            <span>${card.comments}</span>
-                        </div>
-                        <div class="data-wrap">
-                            <p>Downloads: </p>
-                            <span>${card.downloads}</span>
-                        </div>
+    cards.forEach(card => {
+        const cardHTML =
+            `<li class="card"> 
+                <a class="card-link" href="${card.largeImageURL}">
+                    <img class="card-img" src="${card.webformatURL}" alt="${card.tags}" data-source="${card.largeImageURL}" />
+                </a>
+                <div class="card-data">
+                    <div class="data-wrap">
+                        <p>Likes: </p>
+                        <span>${card.likes}</span>
                     </div>
-                 </li>`;
-              gallery.innerHTML += cardHTML;
-              
-        })
-    }
+                    <div class="data-wrap">
+                        <p>Views: </p>
+                        <span>${card.views}</span>
+                    </div>
+                    <div class="data-wrap">
+                        <p>Comments: </p>
+                        <span>${card.comments}</span>
+                    </div>
+                    <div class="data-wrap">
+                        <p>Downloads: </p>
+                        <span>${card.downloads}</span>
+                    </div>
+                </div>
+             </li>`;
+        gallery.insertAdjacentHTML('beforeend', cardHTML);
+    });
+    const lightbox = new SimpleLightbox(".cards a", { captionsData: "alt", captionDelay: 250, captionPosition: 'bottom' });
+    lightbox.refresh();
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,79 @@
-import './js/pixabay-api.js';
-import './js/render-functions.js';
+import { getImages } from './js/pixabay-api.js';
+import renderCard from './js/render-functions.js';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+let page = 1;
+let currentSearchQuery = '';
+
+function showLoader() {
+    loader.style.display = 'inline-block';
+}
+
+function hideLoader() {
+    loader.style.display = 'none';
+}
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    const userInput = event.target.elements.search.value.trim();
+
+    if (userInput === "") {
+        iziToast.show({
+            title: 'Error',
+            backgroundColor: '#EF4040',
+            messageColor: '#FFFFFF',
+            titleColor: '#FFFFFF',
+            message: 'Please enter your search!',
+            position: 'bottomRight'
+        });
+        return;
+    }
+
+    currentSearchQuery = userInput;
+    page = 1;
+
+    const images = await getImages(userInput);
+    if (images.length === 0) {
+        iziToast.show({
+            title: 'Error',
+            backgroundColor: '#EF4040',
+            messageColor: '#FFFFFF',
+            titleColor: '#FFFFFF',
+            message: 'Sorry, there are no images matching your search. Please try again!',
+            position: 'bottomRight'
+        });
+        return;
+    }
+
+    const gallery = document.querySelector('.cards');
+    gallery.innerHTML = '';
+    renderCard(images);
+    loadMoreBtn.style.display = 'flex';
+    window.scrollTo({
+        top: gallery.offsetTop,
+        behavior: 'smooth'
+    });
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+    page++;
+    const images = await getImages(currentSearchQuery, page);
+    if (images.length === 0) {
+        loadMoreBtn.style.display = 'none';
+        iziToast.info({
+            title: 'Info',
+            message: 'We\'re sorry, but you\'ve reached the end of search results.',
+            position: 'bottomRight'
+        });
+        return;
+    }
+    renderCard(images);
+    window.scrollBy({
+        top: document.querySelector('.card').getBoundingClientRect().height * 2,
+        behavior: 'smooth'
+    });
+});
